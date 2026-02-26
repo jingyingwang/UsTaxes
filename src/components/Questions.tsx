ed3bd6aa5dfe65faa2e56d64c424fb15af2bcc20
garnet <jingyingwjy@gmail.com>
2026-02-26 16:54:35 -0500

    Add Form 8938 questions and FBAR guidance

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -1,6 +1,7 @@
 import { ReactElement, useEffect } from 'react'
 import { Helmet } from 'react-helmet'
-import { Grid, List, ListItem } from '@material-ui/core'
+import { Box, Grid, List, ListItem } from '@material-ui/core'
+import Alert from '@material-ui/lab/Alert'
 import { useDispatch, useSelector, TaxesState } from 'ustaxes/redux'
 import { QuestionTagName, Responses } from 'ustaxes/core/data'
 import { getRequiredQuestions } from 'ustaxes/core/data/questions'
@@ -16,6 +17,10 @@ const emptyQuestions: Responses = {
   FOREIGN_ACCOUNT_EXISTS: false,
   FINCEN_114: false,
   FINCEN_114_ACCOUNT_COUNTRY: '',
+  FORM_8938_REQUIRED: false,
+  FORM_8938_ASSET_CATEGORIES: '',
+  FORM_8938_MAX_VALUE: '',
+  FORM_8938_INCOME_REPORTED: '',
   FOREIGN_TRUST_RELATIONSHIP: false,
   LIVE_APART_FROM_SPOUSE: false
 }
@@ -50,6 +55,8 @@ const Questions = (): ReactElement => {
   })
 
   const currentAnswers: Responses = { ...emptyQuestions, ...currentValues }
+  const showFbarGuidance = currentAnswers.FINCEN_114 ?? false
+  const showForm8938Guidance = currentAnswers.FORM_8938_REQUIRED ?? false
 
   // This form can be rerendered because the global state was modified by
   // another control.
@@ -101,6 +108,28 @@ const Questions = (): ReactElement => {
           ))}
         </List>
       </Grid>
+      {(showFbarGuidance || showForm8938Guidance) && (
+        <Box marginTop={2}>
+          {showFbarGuidance && (
+            <Alert severity="info">
+              FBAR reminder: FinCEN Form 114 is filed separately from your IRS
+              return. If required, file electronically with FinCEN and report
+              the highest value of each foreign account and the country where
+              it is located. UsTaxes does not submit the FBAR for you.
+            </Alert>
+          )}
+          {showForm8938Guidance && (
+            <Box marginTop={2}>
+              <Alert severity="info">
+                Form 8938 reminder: Form 8938 (FATCA) is attached to your Form
+                1040. UsTaxes does not generate Form 8938. Keep the asset
+                categories, maximum value, and income details you entered above
+                so you can complete the form if required.
+              </Alert>
+            </Box>
+          )}
+        </Box>
+      )}
       {navButtons}
     </form>
   )
